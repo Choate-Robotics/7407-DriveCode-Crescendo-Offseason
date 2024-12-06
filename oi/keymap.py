33316cf4ef5750754891ac6a9c51d189fef2759b
Ericxiyinyang <ericxiyin@gmail.com>
2024-12-06 12:10:15 -0500

    added keymap for arm positions

--- oi/keymap.py
+++ oi/keymap.py
@@ -1,15 +1,33 @@
+import wpilib
+import commands2.button
+
 from toolkit.oi import (
     XBoxController,
     LogitechController,
     JoystickAxis,
+    Joysticks,
     DefaultButton,
 )
 
+controllerDRIVER = XBoxController
 
 class Controllers:
-    DRIVER: int
+    DRIVER: int = 0
     OPERATOR: int
+
+    DRIVER_CONTROLLER = wpilib.Joystick(0)
 
 
 class Keymap:
-    pass
+    class Arm:
+        SET_ARM_SPEAKER = commands2.button.JoystickButton(
+            Joysticks.joysticks[Controllers.DRIVER], controllerDRIVER.Y
+        )
+
+        SET_ARM_AMP = commands2.button.JoystickButton(
+            Joysticks.joysticks[Controllers.DRIVER], controllerDRIVER.B
+        )
+
+        SET_ARM_INTAKE = commands2.button.JoystickButton(
+            Joysticks.joysticks[Controllers.DRIVER], controllerDRIVER.A
+        )
